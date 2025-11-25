7f85ae860857833f42f960aa49e1fc35cecaa1d0
Alex Muir <git@alexmuir.co.uk>
2025-11-25 23:09:32 +0000

    Add azurerm backend configuration for tfstate

--- providers.tf
+++ providers.tf
@@ -9,6 +9,12 @@ terraform {
       version = "3.7.2"
     }
   }
+  backend "azurerm" {
+    use_azuread_auth     = true
+    storage_account_name = "sttfstatemgmtuks"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
